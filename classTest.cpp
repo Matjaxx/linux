26c77efd0a216422490134b0b7f4fcfa580be869
Matjaxx <mateodomker@gmail.com>
2022-11-17 11:44:08 +0100

    test

---
+++ classTest.cpp
@@ -0,0 +1,8 @@
+//
+// Created by frita on 17/11/2022.
+//
+
+#include "classTest.h"
+void classTest::getadd() {
+    std::cout<<a+b<<std::endl;
+}
---
+++ classTest.h
@@ -0,0 +1,20 @@
+//
+// Created by frita on 17/11/2022.
+//
+#include <iostream>
+#ifndef LINUX_CLASSTEST_H
+#define LINUX_CLASSTEST_H
+
+
+class classTest {
+private:
+    int a =2;
+    int b=3;
+public:
+    void getadd();
+
+
+};
+
+
+#endif //LINUX_CLASSTEST_H
